04415401695c01d5e7df994f8849f96880a95c91
Yangxiu123321 <13390560336@163.com>
2019-02-23 18:47:15 +0800

    shell提交:

--- commit.sh
+++ commit.sh
@@ -2,6 +2,7 @@
 
 git config --global user.email "13390560336@163.com"
 git config --global user.name "Yangxiu123321"
+#git config --global push.default sample
 
 echo -e "\033[32m 修改了的文件: \033[0m"
 git status
@@ -17,7 +18,6 @@ if [ "$choose" == "y" ]||[ "$choose" == "y" ];then
 	read -p "请输入需要提交的分支名称(Current branch):" branchName
 	if [ "$branchName" != "" ];then
 		echo -e "\033[32m 正在push:git push origin $branchName \033[0m"
-		git config --global push.default sample
 		git push
 	else
 		git push origin $branchName
--- main.cpp
+++ main.cpp
@@ -4,11 +4,25 @@ using namespace Eigen;
 using namespace std;
 int main()
 {
-    Matrix<double,2,3> A;
-    A << 1,2,3,
-        4,5,6;
-    cout << "源矩阵" << endl << A << endl;
-    cout << "矩阵转置" << endl<< A.transpose() << endl;
+    Matrix<double,3,3> A;
+    Matrix<double,3,3> B;
+    Matrix<double,3,3> C;
 
+    A << 1,2,3,
+        4,5,6,
+        7,8,9;
+    
+    B << 1,2,3,
+        4,5,6,
+        7,8,9;
+
+    C << 1,2,-4,
+        -2,2,1,
+        -3,4,-2;
+
+    cout << "源矩阵:" << endl << A << endl;
+    cout << "矩阵转置:" << endl << A.transpose() << endl;
+    cout <<  "逆矩阵:" << endl << A.inverse() << endl;
+    cout << "矩阵C行列式的值：" << endl << C.determinant() << endl;
     return 0;
 }
